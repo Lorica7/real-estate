3d3e0caed80769325a99a11b21160a92f83ac8e2
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2022-07-20 11:44:46 -0400

    add new users

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import {
-  ReactComponent as ArrowRightIcon,
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { db } from '../firebase.config';
+ import {ReactComponent as ArrowRightIcon,
 } from '../assets/svg/keyboardArrowRightIcon.svg';
 
 import {
@@ -27,6 +28,28 @@ const SignUp = () => {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const navigate = useNavigate();
 
   return (
@@ -38,15 +61,14 @@ const SignUp = () => {
           </p>
         </header>
         <main>
-          <form >
+          <form onSubmit={onSubmit}>
             <input
-  type="text"
-  className="nameInput"
-  placeholder="name"
-  id="name"
-  value={name}
-  onChange={onChange}
-/>
+              type="text"
+              className="nameInput"
+              placeholder="name"
+              id="name"
+              value={name}
+              onChange={onChange}/>
 
             <input type="email" className="emailInput" placeholder="email" id="email"
               value={email} onChange={onChange}
